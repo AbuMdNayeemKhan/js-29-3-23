f9f38bab9e667b16f88e985d2e007cfb29b44960
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2023-03-29 23:43:14 +0600

    learning brack and continew statement in javascript

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 //ternary operator
 
-var ternaryData = (10 < 100) ? "10 100 er theke choto" : "10 100 er theke boro";
-console.log(ternaryData);
+// var ternaryData = (10 < 100) ? "10 100 er theke choto" : "10 100 er theke boro";
+// console.log(ternaryData);
+
+//break and continue
+
+
+for(i = 0; i < 10; i++){
+    if(i == 7){
+        console.log("seven");
+        continue;
+    }
+    console.log(i);
+}
